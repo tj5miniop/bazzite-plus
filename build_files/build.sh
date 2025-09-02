a02549dd3af8fc978e8e927c26c4656664b47a19
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-09-02 22:05:13 +0100

    Update build.sh - enable flatpak

--- build_files/build.sh
+++ build_files/build.sh
@@ -46,19 +46,14 @@ dnf5 -y remove webapp-manager fish btop p7zip p7zip-plugins rar cockpit-networkm
 flatpak uninstall --all 
 dnf5 -y clean all
 
-
-
-
-
-
-#flatpak -y install flathub com.vysp3r.ProtonPlus --system 
-#flatpak -y install flathub com.usebottles.bottles --system
-#flatpak -y install flathub com.github.tchx84.Flatseal --system
-#flatpak -y install flathub com.ranfdev.DistroShelf --system
-#flatpak -y install flathub io.github.Foldex.AdwSteamGtk --system 
-#flatpak -y install flathub com.brave.Browser --system
+flatpak -y install flathub com.vysp3r.ProtonPlus --system 
+flatpak -y install flathub com.usebottles.bottles --system
+flatpak -y install flathub com.github.tchx84.Flatseal --system
+flatpak -y install flathub com.ranfdev.DistroShelf --system
+flatpak -y install flathub io.github.Foldex.AdwSteamGtk --system 
+flatpak -y install flathub com.brave.Browser --system
 # Clean Flatpak Dependencies
-#flatpak -y uninstall --unused --system
+flatpak -y uninstall --unused --system
 
 
 # Set KDE Defaults 
